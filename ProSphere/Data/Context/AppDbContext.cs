5d7735aac86cf3a9a067567749702a7d63ba1359
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-02 11:51:01 +0200

    add dbcontext

---
+++ ProSphere/Data/Context/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProSphere.Data.Configurations;
+using ProSphere.Domain.Entities;
+
+namespace ProSphere.Data.Context
+{
+    public class AppDbContext : CustomIdentityDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<RefreshTokenAuth> RefreshTokens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(typeof(ApplicationUserConfig).Assembly);
+        }
+    }
+}
---
+++ ProSphere/Data/Context/CustomIdentityDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProSphere.Domain.Entities;
+
+namespace ProSphere.Data.Context
+{
+    public abstract class CustomIdentityDbContext : IdentityDbContext
+        <ApplicationUser,
+        IdentityRole,
+        string,
+        IdentityUserClaim<string>,
+        IdentityUserRole<string>,
+        IdentityUserLogin<string>,
+        IdentityRoleClaim<string>,
+        IdentityUserToken>
+    {
+        protected CustomIdentityDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
