fda2bf51f4f469793ee2fa02ce390805ebc2df39
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-03 07:53:44 +0200

    add supabase constants

---
+++ ProSphere/Domain/Constants/SupabaseConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace ProSphere.Domain.Constants
+{
+    public static class SupabaseConstants
+    {
+        public const string PrefixSupaURL = @"https://hmjlpcwwtfoerncdxkrg.supabase.co/storage/v1/object/public/TumorHospitalFiles/";
+        public const int StartIndexOfCorrectPath = 85;
+    }
+}
