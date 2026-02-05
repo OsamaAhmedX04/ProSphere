1e2c8d3db7eeafff20b99658ffe4660dafd03cab
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-05 21:56:04 +0200

    add request for delete account feature to generate otp and being sent to user's mail

---
+++ ProSphere/Features/Authentication/Commands/RequestDeleteAccount/RequestDeleteAccountCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.Authentication.Commands.DeleteAccount
+{
+    public record RequestDeleteAccountCommand(string userId) : IRequest<Result>
+    {
+    }
+}
---
+++ ProSphere/Features/Authentication/Commands/RequestDeleteAccount/RequestDeleteAccountCommandHandler.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ProSphere.Data.Context;
+using ProSphere.Domain.Constants;
+using ProSphere.Domain.Entities;
+using ProSphere.Extensions;
+using ProSphere.ExternalServices.Interfaces.Email;
+using ProSphere.Helpers;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.Authentication.Commands.DeleteAccount
+{
+    public class RequestDeleteAccountCommandHandler : IRequestHandler<RequestDeleteAccountCommand, Result>
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IEmailSenderService _emailSenderService;
+        private readonly AppDbContext _db;
+
+        public RequestDeleteAccountCommandHandler(UserManager<ApplicationUser> userManager, IEmailSenderService emailSenderService, AppDbContext db)
+        {
+            _userManager = userManager;
+            _emailSenderService = emailSenderService;
+            _db = db;
+        }
+
+        public async Task<Result> Handle(RequestDeleteAccountCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByIdAsync(request.userId);
+
+            if (user == null)
+                return Result.Failure("User Not Found", StatusCodes.Status404NotFound);
+
+            var otp = OTPGenerator.GenerateRandomOTP();
+
+            var result = await _userManager.SetAuthenticationTokenAsync(user, TokenInfo.DefaultLoginProvider, TokenInfo.DeleteAccountOTPName, otp);
+
+            if (!result.Succeeded)
+            {
+                var errors = result.ConvertErrorsToDictionary();
+                return Result.ValidationFailure(errors);
+            }
+
+            var token = await _db.UserTokens.FirstOrDefaultAsync(t => t.Name == TokenInfo.DeleteAccountOTPName && t.UserId == user.Id);
+
+            token!.ExpireDate = DateTime.UtcNow.AddMinutes(15);
+
+            await _db.SaveChangesAsync();
+
+            _emailSenderService.SendDeleteAccountOTPMail(user.Email!,otp);
+
+            return Result.Success("We Have Sent OTP In Your Mail");
+        }
+    }
+}
