a54f4c39e08e236894ac6473c61f9b2efbdf370b
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-05 21:56:37 +0200

    add otp generator

---
+++ ProSphere/Helpers/OTPGenerator.cs
@@ -0,0 +1,14 @@
+﻿using ProSphere.Domain.Constants;
+
+namespace ProSphere.Helpers
+{
+    public static class OTPGenerator
+    {
+        public static string GenerateRandomOTP()
+        {
+            string otp = "";
+            otp += RandomDataGenerator.GetRandomString(CharacterSets.numbers, 6);
+            return otp;
+        }
+    }
+}
