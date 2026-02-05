936e8cfc0d79d0b225ee5840f1ecaea023e48f19
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-05 15:30:58 +0200

    add link generator helper

---
+++ ProSphere/Helpers/LinkGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+
+namespace ProSphere.Helpers
+{
+    public static class LinkGenerator
+    {
+        public static string GenerateEmailConfirmationLink(string userId, string token)
+        {
+            var encodedToken = WebUtility.UrlEncode(token);
+
+            var confirmationLink = $"https://app.prosphere.com/confirm-email?userId={userId}&token={encodedToken}";
+
+            return confirmationLink;
+        }
+    }
+}
