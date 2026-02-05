66ea9a8c8a366bc34c6f66b701f8c9a18a34586e
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-05 21:56:54 +0200

    add random data generator

---
+++ ProSphere/Helpers/RandomDataGenerator.cs
@@ -0,0 +1,18 @@
+﻿namespace ProSphere.Helpers
+{
+    public static class RandomDataGenerator
+    {
+        public static string GetRandomString(char[] array, int numberOfChars)
+        {
+            string text = "";
+            Random random = new Random();
+            for (int i = 0; i < numberOfChars; i++)
+            {
+                var randomNumber = random.NextInt64(0, array.Length);
+                char randomChar = array[randomNumber];
+                text += randomChar;
+            }
+            return text;
+        }
+    }
+}
