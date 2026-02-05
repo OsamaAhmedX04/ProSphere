d29044f0ca713b1075e9f99d5b8f933f66e701b9
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-05 15:32:06 +0200

    add confirm email feature

---
+++ ProSphere/Features/Authentication/Commands/ConfirmEmail/ConfirmEmailCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using ProSphere.Features.Authentication.Commands.Login;
+using ProSphere.ResultResponse;
+using ProSphere.Shared.DTOs;
+
+namespace ProSphere.Features.Authentication.Commands.ConfirmEmail
+{
+    public record ConfirmEmailCommand(string userId, string token) : IRequest<Result<AuthenticationTokenDto>>
+    {
+    }
+}
---
+++ ProSphere/Features/Authentication/Commands/ConfirmEmail/ConfirmEmailCommandHandler.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Newtonsoft.Json.Linq;
+using ProSphere.Domain.Entities;
+using ProSphere.Extensions;
+using ProSphere.ExternalServices.Interfaces.Authentication;
+using ProSphere.Features.Authentication.Commands.Login;
+using ProSphere.ResultResponse;
+using ProSphere.Shared.DTOs;
+using System.Net;
+
+namespace ProSphere.Features.Authentication.Commands.ConfirmEmail
+{
+    public class ConfirmEmailCommandHandler : IRequestHandler<ConfirmEmailCommand, Result<AuthenticationTokenDto>>
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IAuthenticationTokenService _authenticationTokenService;
+
+        public ConfirmEmailCommandHandler(UserManager<ApplicationUser> userManager, IAuthenticationTokenService authenticationTokenService)
+        {
+            _userManager = userManager;
+            _authenticationTokenService = authenticationTokenService;
+        }
+
+        public async Task<Result<AuthenticationTokenDto>> Handle(ConfirmEmailCommand command, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByIdAsync(command.userId);
+
+            if (user == null)
+                return Result<AuthenticationTokenDto>.Failure("User Not Found", 404);
+
+            var decodedToken = WebUtility.UrlDecode(command.token);
+
+            var confirmingResult = await _userManager.ConfirmEmailAsync(user, decodedToken);
+
+            if (!confirmingResult.Succeeded)
+            {
+                var errors = confirmingResult.ConvertErrorsToDictionary();
+                return Result<AuthenticationTokenDto>.ValidationFailure(errors);
+            }
+
+            var authenticationTokenResponse = await _authenticationTokenService.GenerateAuthenticationTokens(user, false);
+
+            return Result<AuthenticationTokenDto>.Success(authenticationTokenResponse, "Email Confirmed Successfully");
+        }
+    }
+}
