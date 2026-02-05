dac65a94a9d1ed52fa65bf863f4cb28f4b5ee3da
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-05 15:31:56 +0200

    add change password feature

---
+++ ProSphere/Features/Authentication/Commands/ChangePassword/ChangePasswordCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using ProSphere.Domain.Entities;
+using ProSphere.Extensions;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.Authentication.Commands.ChangePassword
+{
+    public class ChangePasswordCommandHandler : IRequestHandler<ChangePasswordCommand, Result>
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ChangePasswordCommandHandler(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<Result> Handle(ChangePasswordCommand command, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByIdAsync(command.request.UserId);
+
+            if (user == null)
+                return Result.Failure("User Not Found", 404);
+
+            var changePasswordResult = await _userManager
+                .ChangePasswordAsync(user, command.request.CurrentPassword, command.request.NewPassword);
+
+            if(!changePasswordResult.Succeeded)
+            {
+                var errors = changePasswordResult.ConvertErrorsToDictionary();
+                return Result.ValidationFailure(errors);
+            }
+
+            return Result.Success("Password Has Changed Successfully");
+        }
+    }
+}
---
+++ ProSphere/Features/Authentication/Commands/ChangePassword/ChangePasswordRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace ProSphere.Features.Authentication.Commands.ChangePassword
+{
+    public class ChangePasswordRequest
+    {
+        public string UserId { get; set; }
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
