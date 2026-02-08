566714210e7f31515caa1b4a25f086bcbde9d8e5
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-08 11:10:42 +0200

    create verification controller file

---
+++ ProSphere/Features/Verification/Endpoint/VerificationController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProSphere.Features.Verification.Endpoint
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VerificationController : ControllerBase
+    {
+    }
+}
