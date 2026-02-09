55b3cf998e1d20e320bc9549ef3761ee5ff02933
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-10 01:18:47 +0200

    add delete admin feature

---
+++ ProSphere/Features/Admin/Commands/DeleteAdmin/DeleteAdminCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.Admin.Commands.DeleteAdmin
+{
+    public record DeleteAdminCommand(string adminId) : IRequest<Result>
+    {
+    }
+}
---
+++ ProSphere/Features/Admin/Commands/DeleteAdmin/DeleteAdminCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Identity;
+using ProSphere.Domain.Constants;
+using ProSphere.Domain.Entities;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.Admin.Commands.DeleteAdmin
+{
+    public class DeleteAdminCommandHandler : IRequestHandler<DeleteAdminCommand, Result>
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public DeleteAdminCommandHandler(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<Result> Handle(DeleteAdminCommand command, CancellationToken cancellationToken)
+        {
+            var user = await _userManager.FindByIdAsync(command.adminId);
+
+            if (user == null)
+                return Result.Failure("User Not Found", StatusCodes.Status404NotFound);
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+            var role = userRoles.FirstOrDefault();
+
+            if((role == Role.SuperAdmin) || (role != Role.Admin && role != Role.InActiveAdmin))
+                return Result.Failure("User Not Found", StatusCodes.Status404NotFound);
+
+            await _userManager.DeleteAsync(user);
+
+            return Result.Success("Admin Account Has Been Deleted Successfully");
+        }
+    }
+}
