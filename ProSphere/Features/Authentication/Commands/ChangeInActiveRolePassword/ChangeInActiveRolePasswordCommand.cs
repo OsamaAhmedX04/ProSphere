7e1ab2ed1527464516462d320c1ff7c489209915
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-10 01:19:04 +0200

    add change inActive role password

---
+++ ProSphere/Features/Authentication/Commands/ChangeInActiveRolePassword/ChangeInActiveRolePasswordCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using ProSphere.ResultResponse;
+using ProSphere.Shared.DTOs.Authentication;
+
+namespace ProSphere.Features.Authentication.Commands.ChangeInActiveRolePassword
+{
+    public record ChangeInActiveRolePasswordCommand(ChangeInActiveRolePasswordRequest request)
+        : IRequest<Result<AuthenticationTokenDto>>
+    {
+    }
+}
---
+++ ProSphere/Features/Authentication/Commands/ChangeInActiveRolePassword/ChangeInActiveRolePasswordRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace ProSphere.Features.Authentication.Commands.ChangeInActiveRolePassword
+{
+    public class ChangeInActiveRolePasswordRequest
+    {
+        public string UserId { get; set; }
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+        public bool RememberMe { get; set; } = false;
+    }
+}
