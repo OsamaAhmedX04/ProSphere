79938464af9488bbeffa5809c7a1397f889fd97d
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-10 01:17:29 +0200

    add new roles

--- ProSphere/Domain/Constants/Role.cs
+++ ProSphere/Domain/Constants/Role.cs
@@ -2,10 +2,12 @@
 {
     public static class Role
     {
+        public const string SuperAdmin = "SuperAdmin";
         public const string Admin = "Admin";
         public const string Creator = "Creator";
         public const string Investor = "Investor";
         public const string Moderator = "Moderator";
-        public const string Expert = "Expert";
+        public const string InActiveModerator = "InActiveModerator";
+        public const string InActiveAdmin = "InActiveAdmin";
     }
 }
