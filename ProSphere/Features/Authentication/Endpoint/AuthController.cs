016825a7b7ddaa0e536e8fc3cb0b9766eca7bdfc
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-11 11:48:22 +0200

    add auth controller enpoints

--- ProSphere/Features/Authentication/Endpoint/AuthController.cs
+++ ProSphere/Features/Authentication/Endpoint/AuthController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProSphere.Features.Admin.Commands.CreateAdmin;
+using ProSphere.Features.Authentication.Commands.Login;
+using ProSphere.Features.Authentication.Commands.RefreshToken;
 
 namespace ProSphere.Features.Authentication.Endpoint
 {
@@ -6,5 +10,27 @@ namespace ProSphere.Features.Authentication.Endpoint
     [ApiController]
     public class AuthController : ControllerBase
     {
+        private readonly ISender _sender;
+
+        public AuthController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            var command = new LoginCommand(request);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpPost("refresh-token")]
+        public async Task<IActionResult> RefreshToken([FromBody] RefreshTokenRequest request)
+        {
+            var command = new RefreshTokenCommand(request);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
     }
 }
