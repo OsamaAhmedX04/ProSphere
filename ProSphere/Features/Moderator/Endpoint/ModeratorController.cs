b6758143e0634ffb581680a68631d3762bd3ae1b
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-11 11:48:42 +0200

    add moderator controller enpoints

--- ProSphere/Features/Moderator/Endpoint/ModeratorController.cs
+++ ProSphere/Features/Moderator/Endpoint/ModeratorController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProSphere.Features.CreatorSkills.Commands.DeleteSkills;
+using ProSphere.Features.Moderator.Commands.CreateModerator;
+using ProSphere.Features.Moderator.Commands.RecycleModeratorAccount;
 
 namespace ProSphere.Features.Moderator.Endpoint
 {
@@ -6,5 +10,27 @@ namespace ProSphere.Features.Moderator.Endpoint
     [ApiController]
     public class ModeratorController : ControllerBase
     {
+        private readonly ISender _sender;
+
+        public ModeratorController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateModerator(CreateModeratorRequest request)
+        {
+            var command = new CreateModeratorCommand(request);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpPut("{moderatorId}")]
+        public async Task<IActionResult> RecycleModeratorAccount(string moderatorId)
+        {
+            var command = new RecycleModeratorAccountCommand(moderatorId);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
     }
 }
