4d8fd99f3f58ef8cbeeade8d7bfba46fd0a1c796
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-11 11:49:07 +0200

    add password recovery controller enpoints

--- ProSphere/Features/PasswordRecovery/Endpoint/PasswordRecoveryController.cs
+++ ProSphere/Features/PasswordRecovery/Endpoint/PasswordRecoveryController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProSphere.Features.PasswordManagement.Commands.ChangePassword;
+using ProSphere.Features.PasswordRecovery.Commands.ForgotPasswrod;
+using ProSphere.Features.PasswordRecovery.Commands.ResendResetPasswordToken;
+using ProSphere.Features.PasswordRecovery.Commands.ResetPassword;
 
 namespace ProSphere.Features.PasswordRecovery.Endpoint
 {
@@ -6,5 +11,35 @@ namespace ProSphere.Features.PasswordRecovery.Endpoint
     [ApiController]
     public class PasswordRecoveryController : ControllerBase
     {
+        private readonly ISender _sender;
+
+        public PasswordRecoveryController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost("/api/password-recovery/forgot")]
+        public async Task<IActionResult> ForgotPassword(string email)
+        {
+            var command = new ForgotPasswordCommand(email);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpPut("/api/password-recovery/reset")]
+        public async Task<IActionResult> ResetPassword(string userId, string token, ResetPasswordRequest request)
+        {
+            var command = new ResetPasswordCommand(userId, token, request);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpPost("/api/password-recovery/resend-token")]
+        public async Task<IActionResult> ResendResetPasswordToken(string email)
+        {
+            var command = new ResendResetPasswordTokenCommand(email);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
     }
 }
