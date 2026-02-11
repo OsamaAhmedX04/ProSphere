35d4508193753bebea37ddb931fb401aae1192c4
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-11 11:49:25 +0200

    add registration controller endpoints

--- ProSphere/Features/Registration/Endpoint/RegistrationController.cs
+++ ProSphere/Features/Registration/Endpoint/RegistrationController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProSphere.Features.PasswordRecovery.Commands.ForgotPasswrod;
+using ProSphere.Features.Registration.Commands.ConfirmEmail;
+using ProSphere.Features.Registration.Commands.Register;
+using ProSphere.Features.Registration.Commands.ResendConfirmEmailToken;
 
 namespace ProSphere.Features.Registration.Endpoint
 {
@@ -6,5 +11,35 @@ namespace ProSphere.Features.Registration.Endpoint
     [ApiController]
     public class RegistrationController : ControllerBase
     {
+        private readonly ISender _sender;
+
+        public RegistrationController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(RegisterRequest request)
+        {
+            var command = new RegisterCommand(request);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpPut("confirm-email")]
+        public async Task<IActionResult> ConfirmEmail(string userId, string token)
+        {
+            var command = new ConfirmEmailCommand(userId, token);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpPost("resend-token")]
+        public async Task<IActionResult> ResendConfirmEmailToken(string email)
+        {
+            var command = new ResendConfirmEmailTokenCommand(email);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
     }
 }
