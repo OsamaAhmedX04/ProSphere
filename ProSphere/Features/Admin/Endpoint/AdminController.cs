6ee4d16da09a441f90ebc15b77d102bd6c78aa04
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-11 11:48:13 +0200

    add admin controller enpoints

--- ProSphere/Features/Admin/Endpoint/AdminController.cs
+++ ProSphere/Features/Admin/Endpoint/AdminController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProSphere.Features.Account.Queries.GetAdminAccount;
+using ProSphere.Features.Admin.Commands.CreateAdmin;
+using ProSphere.Features.Admin.Commands.DeleteAdmin;
 
 namespace ProSphere.Features.Admin.Endpoint
 {
@@ -6,5 +10,27 @@ namespace ProSphere.Features.Admin.Endpoint
     [ApiController]
     public class AdminController : ControllerBase
     {
+        private readonly ISender _sender;
+
+        public AdminController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAdmin([FromBody] CreateAdminRequest request)
+        {
+            var command = new CreateAdminCommand(request);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAdmin(string id)
+        {
+            var command = new DeleteAdminCommand(id);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
     }
 }
