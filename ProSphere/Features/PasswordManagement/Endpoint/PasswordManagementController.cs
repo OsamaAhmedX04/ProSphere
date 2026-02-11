fde7ef72ff814b20a8fec51529af28bfb20ae397
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-11 11:48:54 +0200

    add password management controller enpoints

--- ProSphere/Features/PasswordManagement/Endpoint/PasswordManagementController.cs
+++ ProSphere/Features/PasswordManagement/Endpoint/PasswordManagementController.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProSphere.Features.Moderator.Commands.CreateModerator;
+using ProSphere.Features.PasswordManagement.Commands.ChangeInActiveRolePassword;
+using ProSphere.Features.PasswordManagement.Commands.ChangePassword;
 
 namespace ProSphere.Features.PasswordManagement.Endpoint
 {
@@ -6,5 +10,27 @@ namespace ProSphere.Features.PasswordManagement.Endpoint
     [ApiController]
     public class PasswordManagementController : ControllerBase
     {
+        private readonly ISender _sender;
+
+        public PasswordManagementController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPut("/api/password-management")]
+        public async Task<IActionResult> ChangePassword(ChangePasswordRequest request)
+        {
+            var command = new ChangePasswordCommand(request);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpPut("/api/password-management/change-inactive-role")]
+        public async Task<IActionResult> ChangeInActiveRolePassword(ChangeInActiveRolePasswordRequest request)
+        {
+            var command = new ChangeInActiveRolePasswordCommand(request);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
     }
 }
