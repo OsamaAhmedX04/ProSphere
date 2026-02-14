02fc8cfecf7ad75b7d38a954f9906a5708b8f9cd
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-14 11:29:11 +0200

    add delete employee feature

---
+++ ProSphere/Features/Employee/Commands/DeleteEmployee/DeleteEmployeeCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.Employee.Commands.DeleteEmployee
+{
+    public record DeleteEmployeeCommand(Guid employeeId) : IRequest<Result>
+    {
+    }
+}
---
+++ ProSphere/Features/Employee/Commands/DeleteEmployee/DeleteEmployeeCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using ProSphere.RepositoryManager.Interfaces;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.Employee.Commands.DeleteEmployee
+{
+    public class DeleteEmployeeCommandHandler : IRequestHandler<DeleteEmployeeCommand, Result>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteEmployeeCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result> Handle(DeleteEmployeeCommand command, CancellationToken cancellationToken)
+        {
+            var employee = await _unitOfWork.Employees.FirstOrDefaultAsync(e => e.Id == command.employeeId);
+            if (employee is null)
+                return Result.Failure("User Not Found", StatusCodes.Status404NotFound);
+
+            var moderator = await _unitOfWork.Moderators.FirstOrDefaultAsync(m => m.Id == employee.AssignedTo);
+
+            if(moderator is null)
+                return Result.Failure("User Not Found", StatusCodes.Status404NotFound);
+
+            moderator.IsUsed = false;
+            employee.IsActive = false;
+            employee.IsDeleted = true;
+            employee.AssignedTo = null;
+            employee.EndWorkAt = DateTime.UtcNow;
+
+
+            await _unitOfWork.CompleteAsync();
+
+            return Result.Success("Employee Is Deleted Successfully");
+        }
+    }
+}
