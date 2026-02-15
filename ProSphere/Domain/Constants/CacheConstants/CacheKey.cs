f1b2728824307f008872a84e7ea1d7702aa272c5
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-15 10:59:11 +0200

    add cache constant keys

---
+++ ProSphere/Domain/Constants/CacheConstants/CacheKey.cs
@@ -0,0 +1,19 @@
+﻿namespace ProSphere.Domain.Constants.CacheConstants
+{
+    public static class CacheKey
+    {
+        private const string Prefix = "ProSphere";
+
+
+        public const string FinancailDocumentTypes = $"{Prefix}:FinancialDocumentTypes";
+        public const string ProfessionalDocumentTypes = $"{Prefix}:ProfessionalDocumentTypes";
+
+        public static string GetUserSocialMediaAccountsKey(string userId) => $"{Prefix}:UserSocialMediaAccounts:{userId}";
+        public static string GetAdminAccountKey(string userId) => $"{Prefix}:AdminAccount:{userId}";
+        public static string GetCreatorAccountKey(string userId) => $"{Prefix}:CreatorAccount:{userId}";
+        public static string GetInvestorAccountKey(string userId) => $"{Prefix}:CreatorAccount:{userId}";
+        public static string GetModeratorAccountKey(string userId) => $"{Prefix}:ModeratorAccount:{userId}";
+
+
+    }
+}
