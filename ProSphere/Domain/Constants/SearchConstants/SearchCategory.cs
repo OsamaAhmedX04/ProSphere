1f1f421d129e2700e5933d4a2fdfaba193a0770e
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-15 23:38:50 +0200

    add search category constant

---
+++ ProSphere/Domain/Constants/SearchConstants/SearchCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace ProSphere.Domain.Constants.SearchConstants
+{
+    public static class SearchCategory
+    {
+        public const string Investors = "Investors";
+        public const string Creator = "Creator";
+        public const string Project = "Project";
+
+    }
+}
