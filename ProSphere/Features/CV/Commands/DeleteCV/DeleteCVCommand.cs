ae8f99f0bf96785bbf350322866755299893479c
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-18 09:32:35 +0200

    add delete cv feature

---
+++ ProSphere/Features/CV/Commands/DeleteCV/DeleteCVCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.CV.Commands.DeleteCV
+{
+    public record DeleteCVCommand(string UserId) : IRequest<Result>
+    {
+    }
+}
---
+++ ProSphere/Features/CV/Commands/DeleteCV/DeleteCVCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using ProSphere.Domain.Constants.FileConstants;
+using ProSphere.ExternalServices.Interfaces.FileStorage;
+using ProSphere.RepositoryManager.Interfaces;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.CV.Commands.DeleteCV
+{
+    public class DeleteCVCommandHandler : IRequestHandler<DeleteCVCommand, Result>
+    {
+        private readonly IFileService _fileService;
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteCVCommandHandler(IFileService fileService, IUnitOfWork unitOfWork)
+        {
+            _fileService = fileService;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result> Handle(DeleteCVCommand command, CancellationToken cancellationToken)
+        {
+            var user = await _unitOfWork.Creators.GetByIdAsync(command.UserId);
+            if (user is null)
+                return Result.Failure("User Not Found", StatusCodes.Status404NotFound);
+
+            if (user.CVURL == null)
+                return Result.Failure("No CV to delete", StatusCodes.Status400BadRequest);
+
+            await _fileService.DeleteAsync(SupabaseConstants.PrefixSupaURL + user.CVURL);
+            user.CVURL = null;
+
+            await _unitOfWork.CompleteAsync();
+            return Result.Success("CV Deleted Successfully");
+        }
+    }
+}
