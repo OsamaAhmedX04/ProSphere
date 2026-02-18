1ccde24691dfda462a59e7ac3348dbb90514d8b5
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-18 09:32:15 +0200

    add file restrictions on uploading cv file

--- ProSphere/Domain/Constants/FileConstants/FileRestriction.cs
+++ ProSphere/Domain/Constants/FileConstants/FileRestriction.cs
@@ -3,8 +3,13 @@
     public static class FileRestriction
     {
         #region Image
-        public static int AllowableImageFileSize = 1 * 1024 * 1024; // 1MB
+        public static int AllowableImageFileSize = 1 * 1024 * 1024;
         public static List<string> AllowableImageExtensions = new List<string> { ".jpg", ".jpeg", ".png" };
+        #endregion
+
+        #region CVFile
+        public static int AllowableCVFileSize = 3 * 1024 * 1024;
+        public static List<string> AllowableCVFileExtensions = new List<string> { ".pdf", ".docx" };
         #endregion
     }
 }
