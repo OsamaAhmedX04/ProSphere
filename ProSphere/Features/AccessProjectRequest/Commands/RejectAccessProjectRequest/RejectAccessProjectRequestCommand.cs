7a1b8d97eb4a36153f883259bda955886ad9b93a
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-25 23:08:47 +0200

    add reject access project request feature

---
+++ ProSphere/Features/AccessProjectRequest/Commands/RejectAccessProjectRequest/RejectAccessProjectRequestCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.AccessProjectRequest.Commands.RejectAccessProjectRequest
+{
+    public record RejectAccessProjectRequestCommand(Guid requestId) : IRequest<Result>
+    {
+    }
+}
---
+++ ProSphere/Features/AccessProjectRequest/Commands/RejectAccessProjectRequest/RejectAccessProjectRequestCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using ProSphere.Domain.Enums;
+using ProSphere.RepositoryManager.Interfaces;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.AccessProjectRequest.Commands.RejectAccessProjectRequest
+{
+    public class RejectAccessProjectRequestCommandHandler : IRequestHandler<RejectAccessProjectRequestCommand, Result>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public RejectAccessProjectRequestCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result> Handle(RejectAccessProjectRequestCommand command, CancellationToken cancellationToken)
+        {
+            var accessRequest = await _unitOfWork.ProjectsAccessRequests.FirstOrDefaultAsync(x => x.Id == command.requestId);
+            if (accessRequest == null) return Result.Failure("Request Not Exist", StatusCodes.Status404NotFound);
+
+            if (accessRequest.Status != Status.Pending)
+                return Result.Failure($"Access Request Is Already {accessRequest.Status.ToString()}", StatusCodes.Status400BadRequest);
+
+            accessRequest.Status = Status.Rejected;
+            await _unitOfWork.CompleteAsync();
+
+            return Result.Success("Request Is Rejected Successfully");
+        }
+    }
+}
