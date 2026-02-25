9f54d469161d03bafc7a4cbe32520a3ae4e198cf
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-25 23:10:55 +0200

    create projectaccessrequest controller file

---
+++ ProSphere/Features/AccessProjectRequest/Endpoint/ProjectAccessRequestController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProSphere.Features.AccessProjectRequest.Endpoint
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectAccessRequestController : ControllerBase
+    {
+    }
+}
