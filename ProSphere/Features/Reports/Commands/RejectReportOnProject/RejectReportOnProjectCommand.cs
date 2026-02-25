cfb84121b3b55caace1b75b7b4cd0610b1b9f19e
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-25 23:11:31 +0200

    add reject report on project feature

---
+++ ProSphere/Features/Reports/Commands/RejectReportOnProject/RejectReportOnProjectCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.Reports.Commands.RejectReportOnProject
+{
+    public record RejectReportOnProjectCommand(string moderatorId, Guid reportId) : IRequest<Result>
+    {
+    }
+}
---
+++ ProSphere/Features/Reports/Commands/RejectReportOnProject/RejectReportOnProjectCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ProSphere.Domain.Enums;
+using ProSphere.RepositoryManager.Interfaces;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.Reports.Commands.RejectReportOnProject
+{
+    public class RejectReportOnProjectCommandHandler : IRequestHandler<RejectReportOnProjectCommand, Result>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public RejectReportOnProjectCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result> Handle(RejectReportOnProjectCommand command, CancellationToken cancellationToken)
+        {
+            var report = await _unitOfWork.ReportedProjects.FirstOrDefaultAsync(r => r.Id == command.reportId);
+            if (report is null)
+                return Result.Failure("Report Not Found", StatusCodes.Status404NotFound);
+
+            report.Status = Status.Rejected;
+            report.ReviewedAt = DateTime.UtcNow;
+            report.ReviewedBy = command.moderatorId;
+
+            try
+            {
+                await _unitOfWork.CompleteAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return Result.Failure("Error: Another Moderator Took An Action", StatusCodes.Status409Conflict);
+            }
+
+            return Result.Success("Project Report Rejected Successfully");
+        }
+    }
+}
