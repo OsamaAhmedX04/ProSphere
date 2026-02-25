022517e038ad98749851bfcdf14de2371d2462b2
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-25 23:07:02 +0200

    add delete vote on project feature

---
+++ ProSphere/Features/ProjectVoting/Commands/DeleteVote/DeleteVoteCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.ProjectVoting.Commands.DeleteVote
+{
+    public record DeleteVoteCommand(string creatorId, Guid projectId) : IRequest<Result>
+    {
+    }
+}
---
+++ ProSphere/Features/ProjectVoting/Commands/DeleteVote/DeleteVoteCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using ProSphere.Domain.Entities;
+using ProSphere.RepositoryManager.Interfaces;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.ProjectVoting.Commands.DeleteVote
+{
+    public class DeleteVoteCommandHandler : IRequestHandler<DeleteVoteCommand, Result>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteVoteCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result> Handle(DeleteVoteCommand command, CancellationToken cancellationToken)
+        {
+            var isCreatorExist = await _unitOfWork.Creators.IsExistAsync(command.creatorId);
+            if (!isCreatorExist) return Result.Failure("Creator Not Found", StatusCodes.Status404NotFound);
+
+            var isProjectExist = await _unitOfWork.Projects.IsExistAsync(command.projectId);
+            if (!isProjectExist) return Result.Failure("Project Not Found", StatusCodes.Status404NotFound);
+
+            
+            await _unitOfWork.ProjectsVotes.DeleteAsync(vote => 
+                                                        vote.CreatorId == command.creatorId && vote.ProjectId == command.projectId);
+            await _unitOfWork.CompleteAsync();
+
+            return Result.Success("Vote Removed Successfully");
+        }
+    }
+}
