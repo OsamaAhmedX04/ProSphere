c9fdcce1821453c4db8331eaf202015cbe5dfcc4
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-25 23:08:29 +0200

    add delete access project request feature

---
+++ ProSphere/Features/AccessProjectRequest/Commands/DeleteAccessProjectRequest/DeleteAccessProjectRequestCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.AccessProjectRequest.Commands.DeleteAccessProjectRequest
+{
+    public record DeleteAccessProjectRequestCommand(Guid requestId) : IRequest<Result>
+    {
+    }
+}
---
+++ ProSphere/Features/AccessProjectRequest/Commands/DeleteAccessProjectRequest/DeleteAccessProjectRequestCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using ProSphere.Domain.Enums;
+using ProSphere.RepositoryManager.Interfaces;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.AccessProjectRequest.Commands.DeleteAccessProjectRequest
+{
+    public class DeleteAccessProjectRequestCommandHandler : IRequestHandler<DeleteAccessProjectRequestCommand, Result>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteAccessProjectRequestCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result> Handle(DeleteAccessProjectRequestCommand command, CancellationToken cancellationToken)
+        {
+            var accessRequest = await _unitOfWork.ProjectsAccessRequests.FirstOrDefaultAsync(x => x.Id == command.requestId);
+            if (accessRequest == null) return Result.Failure("Request Not Exist", StatusCodes.Status404NotFound);
+
+            if (accessRequest.Status != Status.Pending) 
+                return Result.Failure($"Access Request Is Already {accessRequest.Status.ToString()}", StatusCodes.Status400BadRequest);
+
+            _unitOfWork.ProjectsAccessRequests.Delete(accessRequest.Id);
+            await _unitOfWork.CompleteAsync();
+
+            return Result.Success("Access Request Deleted Successfully");
+        }
+    }
+}
