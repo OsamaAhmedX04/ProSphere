b7273fbb6cbddc92407fc9c03c88d44c3a2d54a5
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-28 22:53:16 +0200

    finish project voting controller

--- ProSphere/Features/ProjectVoting/Endpoint/ProjectVotingController.cs
+++ ProSphere/Features/ProjectVoting/Endpoint/ProjectVotingController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProSphere.Features.ProjectModerating.Commands.AcceptProjectCreation;
+using ProSphere.Features.ProjectVoting.Commands.AddVote;
+using ProSphere.Features.ProjectVoting.Commands.DeleteVote;
+using ProSphere.Features.ProjectVoting.Queries.GetAllVotersOnProject;
 
 namespace ProSphere.Features.ProjectVoting.Endpoint
 {
@@ -6,5 +11,35 @@ namespace ProSphere.Features.ProjectVoting.Endpoint
     [ApiController]
     public class ProjectVotingController : ControllerBase
     {
+        private readonly ISender _sender;
+
+        public ProjectVotingController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpGet("/api/vote/{projectId}")]
+        public async Task<IActionResult> GetAllProjectVotes(int pageNumber, Guid projectId)
+        {
+            var query = new GetAllVotersOnProjectQuery(pageNumber, projectId);
+            var result = await _sender.Send(query);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpPost("/api/vote/{projectId}")]
+        public async Task<IActionResult> AddVote(string creatorId, Guid projectId)
+        {
+            var command = new AddVoteCommand(creatorId, projectId);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpDelete("/api/vote/{projectId}")]
+        public async Task<IActionResult> DeleteVote(string creatorId, Guid projectId)
+        {
+            var command = new DeleteVoteCommand(creatorId, projectId);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
     }
 }
