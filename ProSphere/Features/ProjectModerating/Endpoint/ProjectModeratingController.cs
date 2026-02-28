d43971500591873a37da1ca56296eff2c218721b
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-02-28 22:53:03 +0200

    finish project moderation controller

--- ProSphere/Features/ProjectModerating/Endpoint/ProjectModeratingController.cs
+++ ProSphere/Features/ProjectModerating/Endpoint/ProjectModeratingController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProSphere.Features.ProjectManagement.Queries.GetCreatorProjects;
+using ProSphere.Features.ProjectModerating.Commands.AcceptProjectCreation;
+using ProSphere.Features.ProjectModerating.Commands.RejectProjectCreation;
+using ProSphere.Features.ProjectModerating.Queries.GetAllPendingProjects;
+using ProSphere.Features.ProjectModerating.Queries.GetPendingProjectDetails;
 
 namespace ProSphere.Features.ProjectModerating.Endpoint
 {
@@ -6,5 +12,43 @@ namespace ProSphere.Features.ProjectModerating.Endpoint
     [ApiController]
     public class ProjectModeratingController : ControllerBase
     {
+        private readonly ISender _sender;
+
+        public ProjectModeratingController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpGet("/api/project/pending")]
+        public async Task<IActionResult> GetAllPendingProjects(int pageNumber)
+        {
+            var query = new GetAllPendingProjectsQuery(pageNumber);
+            var result = await _sender.Send(query);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet("/api/project/pending/{projectId}/details")]
+        public async Task<IActionResult> GetPendingProjectDetails(Guid projectId)
+        {
+            var query = new GetPendingProjectDetailsQuery(projectId);
+            var result = await _sender.Send(query);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpPut("/api/project/{projectId}/accept")]
+        public async Task<IActionResult> AcceptProject(string moderatorId, Guid projectId)
+        {
+            var command = new AcceptProjectCreationCommand(moderatorId, projectId);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpPut("/api/project/{projectId}/reject")]
+        public async Task<IActionResult> RejectProject(string moderatorId, Guid projectId, RejectProjectCreationRequest request)
+        {
+            var command = new RejectProjectCreationCommand(moderatorId, projectId, request);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
     }
 }
