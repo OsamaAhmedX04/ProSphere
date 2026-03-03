ff8b43d10fa72017c1861ceb71ab86a4fc139558
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-03-03 17:08:08 +0200

    [feat] delete notification by id

---
+++ ProSphere/Features/Notification/Commands/DeleteNotification/DeleteNotificationCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.Notification.Commands.DeleteNotification
+{
+    public record DeleteNotificationCommand(string userId, Guid notificationId) : IRequest<Result>
+    {
+    }
+}
---
+++ ProSphere/Features/Notification/Commands/DeleteNotification/DeleteNotificationCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using ProSphere.RepositoryManager.Interfaces;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.Notification.Commands.DeleteNotification
+{
+    public class DeleteNotificationCommandHandler : IRequestHandler<DeleteNotificationCommand, Result>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteNotificationCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result> Handle(DeleteNotificationCommand command, CancellationToken cancellationToken)
+        {
+            var isNotificationExist = await _unitOfWork.Notifications
+                .AnyAsync(n => n.UserId == command.userId && n.Id == command.notificationId);
+
+            if (!isNotificationExist) return Result.Failure("Notification Not Exist", StatusCodes.Status404NotFound);
+
+            _unitOfWork.Notifications.Delete(command.notificationId);
+
+            await _unitOfWork.CompleteAsync();
+
+            return Result.Success("Notification Deleted Successfully");
+        }
+    }
+}
