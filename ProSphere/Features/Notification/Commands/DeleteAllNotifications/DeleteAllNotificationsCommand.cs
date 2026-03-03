866b5b630a1433206d79fec19c7b130920dc6bb9
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-03-03 17:07:53 +0200

    [feat] delete all notifications

---
+++ ProSphere/Features/Notification/Commands/DeleteAllNotifications/DeleteAllNotificationsCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.Notification.Commands.DeleteAllNotifications
+{
+    public record DeleteAllNotificationsCommand(string userId) : IRequest<Result>
+    {
+    }
+}
---
+++ ProSphere/Features/Notification/Commands/DeleteAllNotifications/DeleteAllNotificationsCommandHandler.cs
@@ -0,0 +1,22 @@
+﻿using MediatR;
+using ProSphere.RepositoryManager.Interfaces;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.Notification.Commands.DeleteAllNotifications
+{
+    public class DeleteAllNotificationsCommandHandler : IRequestHandler<DeleteAllNotificationsCommand, Result>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public DeleteAllNotificationsCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result> Handle(DeleteAllNotificationsCommand command, CancellationToken cancellationToken)
+        {
+            await _unitOfWork.Notifications.BulkDeleteAsync(n => n.UserId == command.userId);
+            return Result.Success("All Notifications Deleted Successfully");
+        }
+    }
+}
