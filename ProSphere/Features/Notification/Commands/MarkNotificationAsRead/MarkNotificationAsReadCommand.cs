ee17d42ae30e37bbd37d4b792874d764ee5135ff
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-03-03 17:08:27 +0200

    [feat] mark notification as read

---
+++ ProSphere/Features/Notification/Commands/MarkNotificationAsRead/MarkNotificationAsReadCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.Notification.Commands.MarkNotificationAsRead
+{
+    public record MarkNotificationAsReadCommand(string userId, Guid notificationId) : IRequest<Result>
+    {
+    }
+}
---
+++ ProSphere/Features/Notification/Commands/MarkNotificationAsRead/MarkNotificationAsReadCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using ProSphere.RepositoryManager.Interfaces;
+using ProSphere.ResultResponse;
+
+namespace ProSphere.Features.Notification.Commands.MarkNotificationAsRead
+{
+    public class MarkNotificationAsReadCommandHandler : IRequestHandler<MarkNotificationAsReadCommand, Result>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public MarkNotificationAsReadCommandHandler(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Result> Handle(MarkNotificationAsReadCommand command, CancellationToken cancellationToken)
+        {
+            var notification = await _unitOfWork.Notifications
+                .FirstOrDefaultAsync(n => n.Id == command.notificationId && n.UserId == command.userId);
+
+            if (notification == null) return Result.Failure("Notification Not Exist", StatusCodes.Status404NotFound);
+
+            notification.IsSeen = true;
+            notification.SeenAt = DateTime.UtcNow;
+
+            await _unitOfWork.CompleteAsync();
+
+            return Result.Success("Notification Read Successfully");
+        }
+    }
+}
