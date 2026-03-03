115e655d12d8de19ce097d4d5fd96d224cf62f57
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-03-03 17:09:14 +0200

    finish notification controller

--- ProSphere/Features/Notification/Endpoint/NotificationController.cs
+++ ProSphere/Features/Notification/Endpoint/NotificationController.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProSphere.Features.Notification.Commands.DeleteAllNotifications;
+using ProSphere.Features.Notification.Commands.DeleteNotification;
+using ProSphere.Features.Notification.Commands.MarkNotificationAsRead;
+using ProSphere.Features.Notification.Queries.GetAllNotifications;
+using ProSphere.Features.Search.Queries.SearchForProject;
+using System.Security.Claims;
 
 namespace ProSphere.Features.Notification.Endpoint
 {
@@ -6,5 +13,43 @@ namespace ProSphere.Features.Notification.Endpoint
     [ApiController]
     public class NotificationController : ControllerBase
     {
+        private readonly ISender _sender;
+
+        public NotificationController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpGet("{userId}")]
+        public async Task<IActionResult> GetAllNotifications(int pageNumber, string userId)
+        {
+            var query = new GetAllNotificationsQuery(pageNumber, userId);
+            var result = await _sender.Send(query);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpPut("{notificationId}")]
+        public async Task<IActionResult> MarkNotificationAsRead(string userId, Guid notificationId)
+        {
+            var command = new MarkNotificationAsReadCommand(userId, notificationId);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpDelete("{userId}/{notificationId}")]
+        public async Task<IActionResult> DeleteNotification(string userId, Guid notificationId)
+        {
+            var command = new DeleteNotificationCommand(userId, notificationId);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpDelete("{userId}")]
+        public async Task<IActionResult> DeleteAllNotifications(string userId)
+        {
+            var command = new DeleteAllNotificationsCommand(userId);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
     }
 }
