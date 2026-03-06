a12966073fbdfe941e27add5315913f1864fde86
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-03-06 23:03:31 +0200

    finish chat controller

--- ProSphere/Features/Chat/Endpoint/ChatController.cs
+++ ProSphere/Features/Chat/Endpoint/ChatController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using ProSphere.Features.Chat.Commands.SendMessage;
+using ProSphere.Features.Chat.Queries.GetAllChatBetweenUsers;
+using ProSphere.Features.Chat.Queries.GetAllContactsForUser;
+using ProSphere.Features.CreatorSkills.Queries.GetCreatorSkills;
 
 namespace ProSphere.Features.Chat.Endpoint
 {
@@ -6,5 +11,35 @@ namespace ProSphere.Features.Chat.Endpoint
     [ApiController]
     public class ChatController : ControllerBase
     {
+        private readonly ISender _sender;
+
+        public ChatController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpGet("contacts")]
+        public async Task<IActionResult> GetUserContacts(int pageNumber, string userId, string? contactName = null)
+        {
+            var query = new GetAllContactsForUserQuery(pageNumber, userId, contactName);
+            var result = await _sender.Send(query);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpGet("messages")]
+        public async Task<IActionResult> GetChatContent(string firstUserId, string secondUserId, int pageNumber)
+        {
+            var query = new GetAllChatBetweenUsersQuery(firstUserId, secondUserId, pageNumber);
+            var result = await _sender.Send(query);
+            return StatusCode(result.StatusCode, result);
+        }
+
+        [HttpPost("message")]
+        public async Task<IActionResult> SendMessage(string senderId, string receiverId, [FromForm] SendMessageRequest request)
+        {
+            var command = new SendMessageCommand(senderId, receiverId, request);
+            var result = await _sender.Send(command);
+            return StatusCode(result.StatusCode, result);
+        }
     }
 }
