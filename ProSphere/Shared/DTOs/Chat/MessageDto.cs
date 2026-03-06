2d153e39ba654900960efea414a19493d952e09a
OsamaAhmedX04 <osama.smsma999@gmail.com>
2026-03-06 23:03:09 +0200

    add message dto in shared layer

---
+++ ProSphere/Shared/DTOs/Chat/MessageDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ProSphere.Shared.DTOs.Chat
+{
+    public class MessageDto
+    {
+        public string? Message { get; set; }
+        public string? ImageURL { get; set; }
+        public DateTime SentAt { get; set; }
+    }
+}
